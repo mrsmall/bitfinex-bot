3cfd886e818379fd7bd41398ba39e434241efc43
mresc <a.klein@klein-dt.com>
2017-10-16 18:35:37 +0200

    trading logic for long positions

--- src/main/java/com/klein/btc/bitfinex/Bot.java
+++ src/main/java/com/klein/btc/bitfinex/Bot.java
@@ -3,9 +3,13 @@ package com.klein.btc.bitfinex;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.klein.ta.PriceType;
+import com.klein.ta.Series;
+import com.klein.ta.Timeframe;
 import org.java_websocket.client.WebSocketClient;
 import org.java_websocket.drafts.Draft_6455;
 import org.java_websocket.handshake.ServerHandshake;
+import org.joda.time.LocalDateTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -21,7 +25,15 @@ public class Bot extends WebSocketClient {
     private TypeReference<HashMap<String,Object>> typeRef = new TypeReference<HashMap<String,Object>>() {};
     private String pair;
     private List<Integer> subscriptions =new ArrayList<>();
-    private String timeframe;
+    private Timeframe timeframe;
+    private Map<Integer, Series> series=new HashMap<>();
+    private int maxBars=10;
+    private int fastSmaPeriod=10;
+    private int slowSmaPeriod=30;
+    private int trendSmaPeriod=60;
+    private double balance=1000;
+    private double orderSize=1000;
+    private double position=0;
 
 
     private Bot(URI serverUri, Map<String,String> httpHeaders) {
@@ -38,14 +50,20 @@ public class Bot extends WebSocketClient {
         }
     }
 
-    public Bot(String pair, String timeframe) {
+    public Bot(String pair, Timeframe timeframe, int fastSmaPeriod, int slowSmaPeriod, int trendSmaPeriod, double orderSize) {
         this(URI.create("wss://api.bitfinex.com:443/ws/2"), new HashMap<>());
         this.pair=pair;
         this.timeframe = timeframe;
+        this.fastSmaPeriod=fastSmaPeriod;
+        this.slowSmaPeriod=slowSmaPeriod;
+        this.trendSmaPeriod=trendSmaPeriod;
+        this.orderSize=orderSize;
+        maxBars=Math.max(fastSmaPeriod, slowSmaPeriod);
+        maxBars=Math.max(slowSmaPeriod, trendSmaPeriod);
     }
 
     public static void main(String[] args){
-        new Bot("BTCUSD", "1m");
+        new Bot("BTCUSD", Timeframe.M1, 5, 10, 30, 0.02);
     }
 
     @Override
@@ -64,14 +82,20 @@ public class Bot extends WebSocketClient {
                 LOG.trace("Payload: {}", payload);
                 // check if not hb (heart beat) message
                 if (!payload.equals("\"hb\"")){
-                    payload=payload.substring(channelEndPos+2, payload.length()-2);
-                    LOG.trace("Channel update: {}", message);
+                    payload=payload.substring(1, payload.length()-1);
+                    boolean history = false;
+                    if (payload.charAt(0)=='['){
+                        payload=payload.substring(1, payload.length()-1);
+                        history=true;
+                    }
+                    LOG.trace("Channel update: {}", payload);
                     // split candles
                     String[] candles = payload.split("\\],\\[");
-                    for (String candle : candles) {
+                    for (int i=candles.length-1;i>=0;i--) {
+                        String candle=candles[i];
                         //split candle fields: timestamp,open,close,high,low,volume
                         String[] parts = candle.split(",");
-                        processCandle(channelId, Long.parseLong(parts[0]),Double.parseDouble(parts[1]), Double.parseDouble(parts[3]), Double.parseDouble(parts[4]), Double.parseDouble(parts[2]),Double.parseDouble(parts[5]));
+                        processCandle(channelId, Long.parseLong(parts[0]),Double.parseDouble(parts[1]), Double.parseDouble(parts[3]), Double.parseDouble(parts[4]), Double.parseDouble(parts[2]),Double.parseDouble(parts[5]), history);
                     }
                 }
             }
@@ -96,8 +120,48 @@ public class Bot extends WebSocketClient {
      * @param close
      * @param volume
      */
-    private void processCandle(int channelId, long ts, double open, double high, double low, double close, double volume) {
+    private void processCandle(int channelId, long ts, double open, double high, double low, double close, double volume, boolean history) {
+        LocalDateTime date = LocalDateTime.fromDateFields(new Date(ts));
+        LOG.debug("Candle timestamp: {}", date);
 
+        Series s=getSeries(channelId);
+        s.addBar(date, open, high,low,close,volume);
+        LOG.debug("Series: {}, lastIndex: {}", s.getSymbol(), s.getLastIndex());
+
+        if (!history && s.isLastDate(date) && s.getClose().length>maxBars){
+            double[] smaFast = s.sma(fastSmaPeriod);
+            double[] smaSlow = s.sma(slowSmaPeriod);
+            double[] smaTrend= s.sma(trendSmaPeriod);
+
+//            LOG.trace("SMA trend values: {}", Arrays.toString(smaTrend));
+
+            double smaFastLast = smaFast[smaFast.length - 1];
+            double smaSlowLast = smaSlow[smaSlow.length - 1];
+            double smaFastPrev = smaFast[smaFast.length - 2];
+            double smaSlowPrev = smaSlow[smaSlow.length - 2];
+            double smaTrendLast = smaSlow[smaSlow.length - 1];
+            double smaTrendPrev = smaSlow[smaSlow.length - 2];
+
+            boolean smaSlowIsHigherAsTrend=smaSlowLast>smaTrendLast;
+            LOG.info("SMA slow is > SMA trend: {}", smaSlowIsHigherAsTrend);
+            boolean smaFastIsHigherAsSlow = smaFastLast > smaSlowLast;
+            boolean smaFastWasLowOrEqualAsSlow = smaFastPrev<=smaSlowPrev;
+            LOG.info("SMA fast is > SMA slow: {}", smaFastIsHigherAsSlow);
+            LOG.info("SMA fast was <= SMA slow: {}", smaFastWasLowOrEqualAsSlow);
+            if ( smaFastIsHigherAsSlow && smaFastWasLowOrEqualAsSlow && smaSlowIsHigherAsTrend){
+                LOG.info("Buy {} {}@{}", orderSize, pair, close);
+                balance-=orderSize*close;
+                position+=orderSize;
+            } else if (position>0 && (!smaFastIsHigherAsSlow || !smaSlowIsHigherAsTrend)) {
+                LOG.info("Close {} {}@{}", position, pair, close);
+                balance+=position*close;
+                position+=position;
+            }
+        }
+    }
+
+    private Series getSeries(int channelId) {
+        return series.get(channelId);
     }
 
     private void processMessage(HashMap<String, Object> event) {
@@ -109,8 +173,10 @@ public class Bot extends WebSocketClient {
                 subscribeChannel();
             }
         } else if (eventType.equals("subscribed")){
-            if (event.get("key").equals("trade:"+timeframe+":t"+pair)){
-                subscriptions.add((int) event.get("chanId"));
+            if (event.get("key").equals("trade:"+timeframe.getBitfinexCode()+":t"+pair)){
+                int channelId = (int) event.get("chanId");
+                subscriptions.add(channelId);
+                series.put(channelId, new Series(pair, timeframe, maxBars+10));
             }
         }
     }
@@ -119,7 +185,7 @@ public class Bot extends WebSocketClient {
         Map<String, Object> msg=new HashMap<>();
         msg.put("event", "subscribe");
         msg.put("channel", "candles");
-        msg.put("key", "trade:"+timeframe+":t"+pair);
+        msg.put("key", "trade:"+timeframe.getBitfinexCode()+":t"+pair);
         send(msg);
     }
 
