12c659f5899d1515d895fcdee366c079bbd4c87d
mresc <a.klein@klein-dt.com>
2017-10-27 10:58:58 +0200

    converted to spring boot application

---
+++ src/main/java/com/klein/btc/BtcApplication.java
@@ -0,0 +1,12 @@
+package com.klein.btc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BtcApplication.class, args);
+	}
+}
