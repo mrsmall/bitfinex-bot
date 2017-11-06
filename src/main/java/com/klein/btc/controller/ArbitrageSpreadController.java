d02be2a3621f69ede22ed41f12113ee2dfa148ee
mresc <a.klein@klein-dt.com>
2017-11-06 19:44:30 +0100

    rudimental spread chart

--- src/main/java/com/klein/btc/controller/ArbitrageSpreadController.java
+++ src/main/java/com/klein/btc/controller/ArbitrageSpreadController.java
@@ -4,6 +4,9 @@ import com.klein.btc.model.Exchange;
 import com.klein.btc.model.Product;
 import com.klein.btc.model.Ticks;
 import com.klein.btc.repository.TicksRepository;
+import com.tictactec.ta.lib.Core;
+import com.tictactec.ta.lib.MInteger;
+import com.tictactec.ta.lib.RetCode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,33 +22,99 @@ public class ArbitrageSpreadController {
 
     @Autowired
     TicksRepository ticksRepository;
+    private Core core=new Core();
 
     @RequestMapping("/{product}/{exchange1}-{exchange2}/{ts1}-{ts2}")
     @ResponseBody
-    public List<Object[]> indexAction(@PathVariable("product") String productName, @PathVariable("exchange1") String exchange1name, @PathVariable("exchange2") String exchange2name, @PathVariable("ts1") long ts1, @PathVariable("ts2") long ts2) {
+    public Object[] indexAction(@PathVariable("product") String productName, @PathVariable("exchange1") String exchange1name, @PathVariable("exchange2") String exchange2name, @PathVariable("ts1") long ts1, @PathVariable("ts2") long ts2) {
         Exchange exchange1=Exchange.valueOf(exchange1name);
         Exchange exchange2=Exchange.valueOf(exchange2name);
         Product product=Product.valueOf(productName);
-        List<Ticks> ticks1=ticksRepository.findById_exchangeAndId_productAndId_timestampBetweenOrderById_timestampDesc(exchange1, product, ts1, ts2, PageRequest.of(0, 1000)).getContent();
-        List<Ticks> ticks2=ticksRepository.findById_exchangeAndId_productAndId_timestampBetweenOrderById_timestampDesc(exchange2, product, ts1, ts2, PageRequest.of(0, 1000)).getContent();
+        List<Ticks> ticks1=ticksRepository.findById_exchangeAndId_productAndId_timestampBetweenOrderById_timestampDesc(exchange1, product, ts1, ts2, PageRequest.of(0, 2000)).getContent();
+        List<Ticks> ticks2=ticksRepository.findById_exchangeAndId_productAndId_timestampBetweenOrderById_timestampDesc(exchange2, product, ts1, ts2, PageRequest.of(0, 2000)).getContent();
 
         int interval=1000;
         SortedMap<Long, Map<Exchange, Ticks>> sorted=combine(ticks1, ticks2);
-        List<Object[]> spread=new ArrayList<>();
+        List<Long> timestamps=new ArrayList<>();
+        List<Float> spread=new ArrayList<>();
         for (Long ts : sorted.keySet()) {
             Map<Exchange, Ticks> timestampTicks = sorted.get(ts);
             float value;
             if (timestampTicks.size()!=2){
-                value=0f;
-                spread.add(new Object[]{ts, value});
+//                value=0f;
+//                for (int i=0;i<60000;i+=interval)
+//                    spread.add(new Object[]{ts+i, value});
             } else {
                 value=(timestampTicks.get(exchange1).getOpenAsk()-timestampTicks.get(exchange2).getOpenBid())*100/timestampTicks.get(exchange2).getOpenBid();
-                spread.addAll(getValues(ts, interval, timestampTicks.get(exchange1), timestampTicks.get(exchange2)));
+                List<Object[]> values = getValues(ts, interval, timestampTicks.get(exchange1), timestampTicks.get(exchange2));
+                for (Object[] objects : values) {
+                    timestamps.add((Long) objects[0]);
+                    spread.add((Float) objects[1]);
+                }
             }
         }
 
-        return spread;
+        int len = spread.size();
+        int stdDevPeriod=len/2;
+        float[] values = new float[len];
+        float[] sma= new float[len];
+        float smaSum=0;
+        float[] bbLow = new float[len];
+        float[] bbHigh= new float[len];
+        List<Float> tmp=new ArrayList<>();
+        for(int i=0;i<len;i++) {
+            float val=spread.get(i);
+            values[i] = val;
+            tmp.add(val);
+            smaSum+=val;
+            if (tmp.size()>stdDevPeriod)
+                smaSum-=tmp.remove(0);
+            if (tmp.size()==stdDevPeriod){
+                float smaVal=smaSum/stdDevPeriod;
+                sma[i]=smaVal;
+                bbLow[i]=smaVal;
+                bbHigh[i]=smaVal;
+            } else{
+                sma[i]=Float.MIN_VALUE;
+                bbLow[i]=Float.MIN_VALUE;
+                bbHigh[i]=Float.MIN_VALUE;
+            }
+        }
+
+        double[] stdDev = new double[len];
+        float[] oldValues =null;
+        MInteger outBegIdx = new MInteger();
+        MInteger outNBElement = new MInteger();
+        RetCode res = core.stdDev(0, len - 1, values, stdDevPeriod, 2D, outBegIdx, outNBElement, stdDev);
+        if (res == RetCode.Success) {
+            // System.out.println(Arrays.toString(values));
+            stdDev=createFullArray(len, outBegIdx, outNBElement, stdDev, Float.MIN_VALUE);
+        }
+        for(int i=stdDevPeriod-1;i<len;i++) {
+            float smaVal=sma[i];
+            double stdDevVal=stdDev[i];
+            bbLow[i]-=stdDevVal;
+            bbHigh[i]+=stdDevVal;
+        }
+
+        return new Object[]{timestamps, spread, sma, bbLow, bbHigh};
     }
+
+    protected double[] createFullArray(int len, MInteger outBegIdx, MInteger outNBElement, double[] roc, float emptyValue) {
+//        System.out.println("outBegIdx: " + outBegIdx.value);
+//        System.out.println("outNBElement: " + outNBElement.value);
+//        System.out.println("IN: " + Arrays.toString(roc));
+        roc = Arrays.copyOf(roc, outNBElement.value);
+//        System.out.println("COPY IN: " + Arrays.toString(roc));
+
+        double[] fullRoc = new double[len];
+        Arrays.fill(fullRoc, 0, outBegIdx.value, emptyValue);
+        System.arraycopy(roc, 0, fullRoc, outBegIdx.value, roc.length);
+//        System.out.println("OUT: " + Arrays.toString(fullRoc));
+        return fullRoc;
+    }
+
+
 
     private List<Object[]> getValues(long ts, int interval, Ticks ticks1, Ticks ticks2) {
         SortedMap<Long, Map<Exchange, Float>> sorted=new TreeMap<>();
--- src/main/java/com/klein/btc/controller/IndexController.java
+++ src/main/java/com/klein/btc/controller/IndexController.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -17,10 +18,31 @@ public class IndexController {
     @Autowired
     TicksRepository ticksRepository;
 
-    @RequestMapping("/index.html")
+    @RequestMapping("/")
     public String indexAction(ModelMap model) {
-        model.addAttribute("name", "TEST");
-        return "index";
+        return "redirect:/spread-BTCUSD-GDAX-BITFINEX-"+(System.currentTimeMillis()-3600000)+"-"+System.currentTimeMillis()+".html";
+    }
+
+    @RequestMapping("/spread-{product}-{exchange1}-{exchange2}-{ts1}-{ts2}.html")
+    public String spreadAction(@PathVariable("product") String productName, @PathVariable("exchange1") String exchange1name, @PathVariable("exchange2") String exchange2name, @PathVariable("ts1") long ts1, @PathVariable("ts2") long ts2, ModelMap model) {
+        model.addAttribute("product", productName);
+        model.addAttribute("exchange1", exchange1name);
+        model.addAttribute("exchange2", exchange2name);
+        model.addAttribute("ts1", ts1);
+        model.addAttribute("ts1_10m", ts2-(10*60*1000));
+        model.addAttribute("ts1_30m", ts2-(30*60*1000));
+        model.addAttribute("ts1_60m", ts2-(60*60*1000));
+        model.addAttribute("ts1_240m", ts2-(240*60*1000));
+        model.addAttribute("ts1_480m", ts2-(480*60*1000));
+        model.addAttribute("ts1_960m", ts2-(960*60*1000));
+        long tsDiff=ts2-ts1;
+        model.addAttribute("ts1_prev", ts1-tsDiff);
+        model.addAttribute("ts1_next", ts1+tsDiff);
+        model.addAttribute("ts2", ts2);
+        model.addAttribute("ts2_prev", ts2-tsDiff);
+        model.addAttribute("ts2_next", ts2+tsDiff);
+        model.addAttribute("ts_diff", tsDiff);
+        return "spread";
     }
 
 }
