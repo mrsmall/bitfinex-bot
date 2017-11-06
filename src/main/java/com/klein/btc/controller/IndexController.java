b67475656bad53ae432f5bb655aa643d61704b80
mresc <a.klein@klein-dt.com>
2017-11-06 15:54:41 +0100

    clean up

--- src/main/java/com/klein/btc/controller/IndexController.java
+++ src/main/java/com/klein/btc/controller/IndexController.java
@@ -1,11 +1,21 @@
 package com.klein.btc.controller;
 
+import com.klein.btc.model.Exchange;
+import com.klein.btc.model.Product;
+import com.klein.btc.model.Ticks;
+import com.klein.btc.repository.TicksRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
+
+    @Autowired
+    TicksRepository ticksRepository;
 
     @RequestMapping("/index.html")
     public String indexAction(ModelMap model) {
